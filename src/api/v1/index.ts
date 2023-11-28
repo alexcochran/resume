89b3de87d251e7b659ebf53d3cf93b3f8329c908
Alex Cochran <acochran50@gmail.com>
2023-11-27 21:04:48 -0700

    add v1 api

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,16 +1,37 @@
-/*  */ import express from 'express';
+import express from 'express';
+
+import routesDownloads from './routes/downloads';
+import routesBasics from './routes/basics';
+import routesEducation from './routes/education';
+import routesInterests from './routes/interests';
+import routesResume from './routes/resume';
+import routesSkills from './routes/skills';
+import routesWork from './routes/work';
 
 import MessageResponse from '../../interfaces/MessageResponse';
-import emojis from './emoji';
+import path from 'path';
 
 const router = express.Router();
 
-router.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏',
-  });
+router.get<{}, MessageResponse>('/docs', (req, res) => {
+  res
+    .set(
+      'Content-Security-Policy',
+      "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"
+    )
+    .sendFile(path.resolve(__dirname, '../../../public/rapidoc.html'));
 });
 
-router.use('/emojis', emojis);
+router.use('/', routesResume);
+
+// Utility endpoints
+router.use('/downloads', routesDownloads);
+
+// JSON endpoints
+router.use('/basics', routesBasics);
+router.use('/education', routesEducation);
+router.use('/interests', routesInterests);
+router.use('/skills', routesSkills);
+router.use('/work', routesWork);
 
 export default router;
