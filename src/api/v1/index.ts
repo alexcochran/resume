8c0b4c70a41a6f2417741b75e687236330a3f724
Alex Cochran <acochran50@gmail.com>
2023-11-28 00:33:01 -0700

    cleanup index

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -9,30 +9,14 @@ import routesResume from './routes/resume';
 import routesSkills from './routes/skills';
 import routesWork from './routes/work';
 
-import path from 'path';
-
 const router = express.Router();
-
-// router.get('/docs', (req, res) => {
-//   res
-//     .set(
-//       'Content-Security-Policy',
-//       "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"
-//     )
-//     .sendFile(path.resolve(__dirname, '../../../../public/rapidoc.html'));
-// });
-
-// router.get('/openapi-spec', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../../../../public/openapi-spec.json'));
-// });
-
-router.use('/', routesResume);
 
 // Utility endpoints
 router.use('/downloads', routesDownloads);
 router.use('/docs', routesDocs);
 
 // JSON endpoints
+router.use('/', routesResume);
 router.use('/basics', routesBasics);
 router.use('/education', routesEducation);
 router.use('/interests', routesInterests);
